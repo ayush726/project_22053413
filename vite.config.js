ac6cc4807eb9b66d83c67a6cca447cfc6b7eb7ae
ayush726 <keshriayush123@gmail.com>
2025-04-09 14:55:50 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/project_22053413/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
